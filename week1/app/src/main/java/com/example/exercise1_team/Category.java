86ccbd404f98422b52a1e178783de9b5adf99bdc
ppk205 <phucpro10204@gmail.com>
2025-11-28 15:13:14 +0700

    added category UI, API call for category

---
+++ week1/app/src/main/java/com/example/exercise1_team/Category.java
@@ -0,0 +1,44 @@
+//code thu thach 5 - hoatd
+//start
+package com.example.giuaki3;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Category {
+
+    @SerializedName("ID")
+    private final int id;
+
+    @SerializedName("ProductName")
+    private final String name;
+
+    @SerializedName("Category")
+    private final String category;
+
+    @SerializedName("ImageURL")
+    private final String imageUrl;
+
+    public Category(int id, String name, String category, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.imageUrl = imageUrl;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
+//end

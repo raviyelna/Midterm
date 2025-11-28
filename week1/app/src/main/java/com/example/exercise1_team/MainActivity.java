5744700038e13e286543399e6c151041d0b2f612
Anh Linh <anhduy15190@gmail.com>
2025-11-28 15:54:17 +0700

    update ui

--- week1/app/src/main/java/com/example/exercise1_team/MainActivity.java
+++ week1/app/src/main/java/com/example/exercise1_team/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.exercise1_team;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -19,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.nn);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);

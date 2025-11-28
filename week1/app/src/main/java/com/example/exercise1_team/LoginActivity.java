2086e5e245fd8b57884ae27966a0670528fd5a8d
KHANG <23162037@student.hcmute.edu.vn>
2025-11-28 12:58:24 +0700

    Initial commit

---
+++ week1/app/src/main/java/com/example/exercise1_team/LoginActivity.java
@@ -0,0 +1,31 @@
+package com.example.exercise1_team;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        Window window = getWindow();
+        WindowInsetsController controller = window.getInsetsController();
+        controller.hide(WindowInsets.Type.statusBars());
+    }
+}
